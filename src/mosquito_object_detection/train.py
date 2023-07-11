ca469f36e358432feb0ebdaf84287cbd6b52b381
Brandon Knape <d.brandonk@gmail.con>
2023-07-10 21:33:43 -0500

    updated path handling in train.py

--- src/mosquito_object_detection/train.py
+++ src/mosquito_object_detection/train.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 from ultralytics import YOLO
 
-dir_path = Path(__file__).resolve().parent
-# dir_path = os.path.dirname(os.path.abspath(__file__))
+# This seems jank. Haven't tried hard enough to find a better way.
+package_path = Path(__file__).resolve().parent.parent.parent
 
 # load a pretrained model
-model = YOLO(dir_path / Path('../../checkpoints/yolov8n.pt'))
+model = YOLO(package_path / Path('checkpoints/yolov8n.pt'))
 
-model.train(data=(dir_path / Path('../../config/mosquito_alert.yaml')), epochs=100, imgsz=640)
+model.train(data=(package_path / Path('config/mosquito_alert.yaml')), epochs=100, imgsz=640)
