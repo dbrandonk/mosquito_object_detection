3052ef92f2636a002bfbc7a6e8afcb69e4e52442
Brandon Knape <d.brandonk@gmail.com>
2023-08-04 06:08:23 -0500

    updated train args.

--- src/mosquito_object_detection/train.py
+++ src/mosquito_object_detection/train.py
@@ -10,4 +10,13 @@ PACKAGE_PATH = Path(__file__).resolve().parent.parent.parent
 model = YOLO(PACKAGE_PATH / Path('weights/yolov8n.pt'))
 
 # train
-model.train(data=(PACKAGE_PATH / Path('config/mosquito_alert.yaml')), epochs=100, imgsz=640)
+model.train(data=(PACKAGE_PATH / Path('config/mosquito_alert.yaml')),
+            batch=16,
+            close_mosaic=10,
+            epochs=300,
+            imgsz=640,
+            lr0=0.001,
+            optimizer='Adam',
+            momentum=0.937,
+            patience=50,
+            pretrained=True)
