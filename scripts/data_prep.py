646e86ca5a269c11c08c342919a578bb91c60791
Brandon Knape <d.brandonk@gmail.com>
2023-09-02 21:34:17 -0500

    supporting classification data struct. using part 2 challenge data.

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -14,13 +14,20 @@ PACKAGE_PATH = Path(__file__).resolve().parent.parent
 DATA_ROOT_PATH = PACKAGE_PATH / Path('data')
 DATA_PATH = DATA_ROOT_PATH / Path('mosquito_alert_2023')
 IMAGE_PATH = DATA_PATH / Path('images')
+GRAB_IMAGE_PATH = IMAGE_PATH / Path('final')
 TRAIN_IMAGE_PATH = IMAGE_PATH / Path('train')
 TRAIN_LABEL_PATH = DATA_PATH / Path('labels/train')
 VAL_IMAGE_PATH = IMAGE_PATH / Path('val')
 VAL_LABEL_PATH = DATA_PATH / Path('labels/val')
+TRAIN_CLASSIFICATION_PATH = DATA_PATH / Path('classification/train')
+VAL_CLASSIFICATION_PATH = DATA_PATH / Path('classification/val')
 CLASS_MAPPING_PATH = PACKAGE_PATH / Path('config/mosquito_alert.yaml')
-TRAINING_DATA_PATH = DATA_PATH / Path('train.csv')
+TRAINING_DATA_PATH = DATA_PATH / Path('phase2_train_v0.csv')
 
+def _clean_up():
+
+    # cleaning up
+    shutil.rmtree(GRAB_IMAGE_PATH)
 
 def _convert_yolo_format(data_frame, class_mapping, balance_data):  # pylint: disable=too-many-locals
 
@@ -42,7 +49,7 @@ def _convert_yolo_format(data_frame, class_mapping, balance_data):  # pylint: di
                 label_placement_path = VAL_LABEL_PATH
 
             shutil.copy(
-                IMAGE_PATH /
+                GRAB_IMAGE_PATH /
                 Path(
                     row['img_fName']),
                 image_placement_path /
@@ -58,10 +65,40 @@ def _convert_yolo_format(data_frame, class_mapping, balance_data):  # pylint: di
                       'w', encoding='utf-8') as file:
                 file.write(f'{class_label} {x_center} {y_center} {width} {height}\n')
 
-    # cleaning up
-    for file_name in shutil.os.listdir(IMAGE_PATH):
-        if file_name.endswith('.jpeg'):
-            shutil.os.remove(IMAGE_PATH / Path(file_name))
+def _org_classification_data(data_frame, class_mapping, balance_data):  # pylint: disable=too-many-locals
+
+    # lets make all of the sub folders.
+    for cls in class_mapping:
+
+        class_label = cls.replace('/', '_') # just in case, otherwise stucture will break.
+
+        # remove any data that might be there.
+        try:
+            shutil.rmtree(TRAIN_CLASSIFICATION_PATH/ Path(class_label))
+            shutil.rmtree(VAL_CLASSIFICATION_PATH/ Path(class_label))
+        except FileNotFoundError:
+            pass
+
+        shutil.os.makedirs(TRAIN_CLASSIFICATION_PATH/ Path(class_label))
+        shutil.os.makedirs(VAL_CLASSIFICATION_PATH/ Path(class_label))
+
+    x_train, x_test = train_test_split(data_frame, test_size=0.2, shuffle=True, random_state=42)
+
+    if balance_data:
+        target = x_train['class_label']
+        oversampler = RandomOverSampler(random_state=42)
+        x_train, _ = oversampler.fit_resample(x_train, target)
+
+    for dataset_idx, dataset in enumerate([x_train, x_test]):
+        for idx, row in dataset.iterrows():
+
+            if dataset_idx == 0:
+                image_placement_path = TRAIN_CLASSIFICATION_PATH
+            else:
+                image_placement_path = VAL_CLASSIFICATION_PATH
+
+            class_label = row['class_label'].replace('/', '_') # just in case, otherwise stucture will break.
+            shutil.copy(GRAB_IMAGE_PATH / Path( row['img_fName']), image_placement_path / Path(class_label + f'/img{idx}.jpeg'))
 
 
 def _get_class_mapping():
@@ -102,24 +139,20 @@ def _refresh_data_folders():
     except FileNotFoundError:
         pass
 
+    shutil.os.makedirs(TRAIN_CLASSIFICATION_PATH)
     shutil.os.makedirs(TRAIN_IMAGE_PATH)
     shutil.os.makedirs(TRAIN_LABEL_PATH)
+    shutil.os.makedirs(VAL_CLASSIFICATION_PATH)
     shutil.os.makedirs(VAL_IMAGE_PATH)
     shutil.os.makedirs(VAL_LABEL_PATH)
 
 
 def _unpack_data():
 
-    zip_file_path = DATA_PATH / Path('train_images.zip')
+    zip_file_path = DATA_PATH / Path('phase2_train_v0.zip')
 
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(IMAGE_PATH)
-
-    zip_file_path = DATA_PATH / Path('test_images_phase1.zip')
-
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        zip_ref.extractall(IMAGE_PATH / Path('test'))
-
 
 def data_prep():
     """
@@ -137,6 +170,9 @@ def data_prep():
     class_mapping = _get_class_mapping()
     data_frame = _read_training_data()
     _convert_yolo_format(data_frame, class_mapping, args.balance_data)
+    _org_classification_data(data_frame, class_mapping, args.balance_data)
+
+    _clean_up()
 
 
 if __name__ == "__main__":
